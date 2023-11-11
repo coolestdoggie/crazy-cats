b40c7d07278b89a345ff284e68a3687b3429274b
maxvone <maximvone@gmail.com>
2023-11-11 14:58:39 +0100

    [ADD]: pushing cat

--- Cats/Cat.gd
+++ Cats/Cat.gd
@@ -1,11 +1,30 @@
 extends RigidBody2D
 
+@export var raycast: RayCast2D
+@export var cooldown_between_pushes: float
+@export var impulse_force: float
 
-# Called when the node enters the scene tree for the first time.
+var current_cooldown: float
+
 func _ready():
-	pass # Replace with function body.
+	current_cooldown = cooldown_between_pushes
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	current_cooldown = current_cooldown - delta
+
+	if not raycast.collide_with_bodies or current_cooldown > 0:
+		return
+	
+	push_raycasted_body()
+
+
+func push_raycasted_body():
+	var rigid_body = (raycast.get_collider() as RigidBody2D)	
+	if !rigid_body:
+		return
+
+	var angle = rotation
+	rigid_body.apply_impulse(Vector2(cos(angle), sin(angle)) * impulse_force)
+
+	current_cooldown = cooldown_between_pushes
+	
