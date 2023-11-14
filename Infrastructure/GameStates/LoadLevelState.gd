e9331438a55e5b720fff2ebcc9cd76b9513ecd7f
maxvone <maximvone@gmail.com>
2023-11-14 12:39:22 +0100

    [ADD]: level instantiating

---
+++ Infrastructure/GameStates/LoadLevelState.gd
@@ -0,0 +1,11 @@
+extends State
+
+@export var levels: Array[PackedScene]
+
+var current_level := 0
+var level_instance
+
+func enter(_msg := {}) -> void:
+	level_instance = levels[0].instantiate()
+	get_tree().root.get_child(0).add_child(level_instance)
+	state_machine.transition_to("PlanningState")
