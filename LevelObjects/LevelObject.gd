ea8bea93401fee2cd6cc14bfef203c459e987e15
maxvone <maximvone@gmail.com>
2023-11-18 10:49:23 +0100

    [REFACTOR]: minor refactor

--- LevelObjects/LevelObject.gd
+++ LevelObjects/LevelObject.gd
@@ -9,16 +9,6 @@ func _ready():
 	mouse_exited.connect(_on_mouse_exited)
 
 
-func _process(delta):
-	if is_dragging and have_drag_ability:
-		global_position = get_global_mouse_position()
-
-	if Input.is_action_just_pressed("click") and is_dragging:
-		is_dragging = false
-	elif Input.is_action_just_pressed("click") and !is_dragging and is_in_collider_bounds:
-		is_dragging = true
-
-
 func _on_mouse_entered():
 	is_in_collider_bounds = true
 	print(is_in_collider_bounds)
@@ -27,3 +17,20 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	is_in_collider_bounds = false
 	print(is_in_collider_bounds)
+
+
+func _process(delta):
+	handle_moving()
+	handle_drag_and_drop()
+
+
+func handle_moving():
+	if is_dragging and have_drag_ability:
+		global_position = get_global_mouse_position()
+
+
+func handle_drag_and_drop():
+	if Input.is_action_just_pressed("click") and is_dragging:
+		is_dragging = false
+	elif Input.is_action_just_pressed("click") and !is_dragging and is_in_collider_bounds:
+		is_dragging = true
